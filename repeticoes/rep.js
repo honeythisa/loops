325b98feef184831b6cd4e79bac479e70aa3d05b
Melissa Medeiros Teodoro <honeythisa@gmail.com>
2021-10-12 19:57:48 -0300

    revisão dos loops
    exemplos simples de base

---
+++ repeticoes/rep.js
@@ -0,0 +1,23 @@
+console.log("-------- while --------")
+let i = 1
+while (i <= 6) {
+    console.log(`Passo ${i}`)
+    i++ // c = c + 1
+}
+console.log(' ')
+console.log("-------- do while --------")
+
+let c = 6
+
+do {
+    console.log(c)
+} while (c <= 5)
+console.log("passou do limite")
+
+console.log(' ')
+console.log("-------- for --------")
+
+for (let contador = 1; contador <= 4; contador++) {
+    console.log(contador)
+}
+console.log("FIM!")
